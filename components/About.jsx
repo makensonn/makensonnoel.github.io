d6e46587f187ea5110b09a0f52e57c49559710cc
Makenson Noel <makensonnoel@Makensons-Air.attlocal.net>
2022-08-25 13:50:35 -0400

    About description changes

--- components/About.jsx
+++ components/About.jsx
@@ -11,6 +11,7 @@ const About = () => {
       <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
         <div className='col-span-2'>
           <p className={titleStyle}><br></br><br></br>About</p>
+          <p className='py-2'></p>
           <p className='py-2 text-gray-600'>
           I am Haitian and I'm a first generation STEM college graduate. I attended Florida Atlantic University, 
           where I obtained a Bachelor of Science in Computer Science degree.
