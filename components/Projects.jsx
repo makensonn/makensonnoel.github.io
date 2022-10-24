0e3c937be5ec5d3c75918b62b630b421f4419b33
Makenson Noel <makensonnoel@Makensons-Air.attlocal.net>
2022-10-23 23:40:12 -0400

    image name fix

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -6,7 +6,7 @@ import React from 'react';
 import materialImg from '../public/assets/projects/material handling robot.png';
 import insightImg from '../public/assets/projects/insightt.png';
 
-//Links
+//Link
 const materialLink ='https://www.youtube.com/watch?v=CdM2Otm6FBI';
 const insightLink ='https://insitee.github.io/';
 
