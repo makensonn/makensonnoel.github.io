5dda7e9f111a978fc47166fdd60e6024f16a4c5f
Makenson Noel <makensonnoel@Makensons-Air.attlocal.net>
2022-11-03 10:14:25 -0400

    Navbar update

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -163,7 +163,7 @@ const Navbar = () => {
                     <FaGithub />
                   </div>
                 </a>
-                <Link href='/#contact'>
+                <Link href='/contact'>
                   <div
                     onClick={() => setNav(!nav)}
                     className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'
