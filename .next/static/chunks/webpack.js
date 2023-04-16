5031ffd4050a68da16363ea7db8ec828c9a5301b
Makenson Noel <22712773+makensonn@users.noreply.github.com>
2023-04-16 23:23:04 +0000

    Contact update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4155e183c8a0ecce"; }
+/******/ 		__webpack_require__.h = function() { return "78aa52620cd605f3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.099f2956a2dc0fcb.hot-update.js
+++ .next/static/webpack/webpack.099f2956a2dc0fcb.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "4155e183c8a0ecce"; }
+/******/ 	__webpack_require__.h = function() { return "78aa52620cd605f3"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
