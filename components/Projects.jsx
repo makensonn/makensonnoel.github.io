b63f38944c214262fa160b86374e032d3f5e6d64
Makenson Noel <22712773+makensonn@users.noreply.github.com>
2023-04-19 20:11:38 +0000

    Guide update

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
-import page from '../pages/guide/page';
+import page from '../pages/guide/guide';
 
 
 //Image
