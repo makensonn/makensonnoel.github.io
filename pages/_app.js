b01a0fdaa8e9b9a1b66b826de15b57e541904459
Makenson Noel <22712773+makensonn@users.noreply.github.com>
2023-06-23 16:23:04 +0000

    Vercel Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import Navbar from '../components/Navbar';
 import '../styles/globals.css';
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Navbar />
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
