22f6244be925a9811592114cc3c9c27aca234fb1
Makenson Noel <22712773+makensonn@users.noreply.github.com>
2023-06-25 02:28:18 +0000

    Tic Tac Toe Game

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ba2d5e1971c5e476"; }
+/******/ 		__webpack_require__.h = function() { return "75d488df4b0d4481"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ tic.jsx
@@ -0,0 +1,21 @@
+import TicTacToe from '../components/TicTacToe';
+
+const TicTacToePage = () => {
+  return (
+    <div>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+      <h1>Tic Tac Toe</h1>
+
+      <TicTacToe />
+    </div>
+  );
+};
+
+export default TicTacToePage;
