b38b284e247b5944e8bbca8099d0c7ee20921e07
Makenson Noel <22712773+makensonn@users.noreply.github.com>
2023-07-08 14:58:21 +0000

    Sudoku Update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "db39e62127c729c8"; }
+/******/ 		__webpack_require__.h = function() { return "19a8c7d5b4103a63"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/sud.jsx
+++ pages/sud.jsx
@@ -22,7 +22,7 @@ const SudokuSolverPage = () => {
         />
         <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
           <h2 className='py-2'>Sudoku Solver</h2>
-          <h3>Java </h3>
+          <h3>JavaScript</h3>
         </div>
       </div>
       {/*Header*/}
@@ -40,56 +40,19 @@ const SudokuSolverPage = () => {
           <br></br><br></br>
           <h2>Overview</h2>
           <p>
-            In this task, I created a Sudoku game using JSX and integrated Tailwind CSS for styling. 
+            Created a Sudoku game using JavaScript and Tailwind CSS. 
             The Sudoku game allows users to input numbers into a 9x9 grid, aiming to complete the grid 
             with numbers from 1 to 9 without any repetition in rows, columns, or 3x3 boxes. 
             The game provides a user-friendly interface for solving Sudoku puzzles.
           </p>
 
-          <br></br><br></br>
-          <h2>Key Features</h2>
-          <p>
-            1. Interactive Grid: The Sudoku grid is rendered dynamically, allowing users to input numbers into each cell. Changes made by the user are reflected in real-time.
-            <br></br><br></br>
-            2. Input Validation: The game validates user input to ensure that only numbers from 1 to 9 are accepted in each cell. Non-numeric inputs or numbers outside the valid range are handled gracefully.
-            <br></br><br></br>
-            3. Responsive Design: The game's UI is designed to be responsive using Tailwind CSS classes, ensuring a consistent experience across different screen sizes and devices.
-          </p>
-
-          <br></br><br></br>
-          <h2>Implementation Details</h2>
-          <div>
-            <li>State Management: The Sudoku board is stored in the component state using the useState hook. Each cell of the board is represented by a 2D array of numbers.</li>
-            <br></br>
-            <li>User Input Handling: The handleCellChange function is responsible for updating the board state when a user enters a number in a cell. It extracts the new value from the input element and updates the corresponding cell in the board state.</li>
-            <br></br>
-            <li>Rendering the Board: The renderBoard function generates the JSX code for rendering the Sudoku board. It iterates over the board state and generates table rows and cells. Each cell contains an input element for user input.</li>
-            <br></br>
-            <li>Styling with Tailwind CSS: Tailwind CSS classes are applied to various elements to style the game's UI. Classes like border, text-center, bg-gray-200, and focus:outline-none are used to achieve the desired appearance.</li>
-          </div>
-
-          <br></br><br></br>
-          <h2>Conclusion</h2>
-          <p>
-            In this task, I successfully created a Sudoku game using JSX and Tailwind CSS. 
-            The game allows users to solve Sudoku puzzles by inputting numbers into a 9x9 grid. 
-            The use of React for component-based UI and Tailwind CSS for styling ensured a clean and responsive design. 
-            This implementation can serve as a foundation for further enhancements, such as implementing puzzle generation, solving algorithms, and game logic validation.
-          </p>
           
           <a
             href='https://github.com/makensonn/makensonn.github.io/blob/master/components/Sudoku.jsx'
             target='_blank'
             rel='noreferrer'
           >
-            <button className='px-8 py-2 mt-4 mr-8'>Game Code</button>
-          </a>
-          <a
-            href='https://github.com/makensonn/Sudoku-solver/blob/master/Sudokusolver.java'
-            target='_blank'
-            rel='noreferrer'
-          >
-            <button className='px-8 py-2 mt-4 mr-8'>Solver Code</button>
+            <button className='px-8 py-2 mt-4 mr-8'>Source Code</button>
           </a>
         </div>
         {/*Project Overview*/}
@@ -100,10 +63,7 @@ const SudokuSolverPage = () => {
             <p className='text-center font-bold pb-2'>Technologies</p>
             <div className='grid grid-cols-3 md:grid-cols-1'>
               <p className='text-gray-600 py-2 flex items-center'>
-                <RiRadioButtonFill className='pr-1' /> Java
-              </p>
-              <p className='text-gray-600 py-2 flex items-center'>
-                <RiRadioButtonFill className='pr-1' /> Next.js
+                <RiRadioButtonFill className='pr-1' /> JavaScript
               </p>
               <p className='text-gray-600 py-2 flex items-center'>
                 <RiRadioButtonFill className='pr-1' /> TailwindCSS
