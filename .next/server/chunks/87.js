00573aa6c6bca9c6620f1f27d2fefe6e71fccd97
Makenson Noel <22712773+makensonn@users.noreply.github.com>
2023-08-03 19:52:37 +0000

    Added the image for the weather app

---
+++ .next/server/chunks/87.js
@@ -0,0 +1,145 @@
+"use strict";
+exports.id = 87;
+exports.ids = [87];
+exports.modules = {
+
+/***/ 966:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({"src":"/_next/static/media/sudoku.ab836383.png","height":1080,"width":1920,"blurDataURL":"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAFCAMAAABPT11nAAAAMFBMVEUAAAB1dXXn6OpfXl7x8vTIyMiGhobQ0NBOTk5QUFC0tLRzc3O7u7v9/f3W19nMztHoNY8qAAAADXRSTlMCnv6j/cQk3HeGxI/dB2QMKwAAAAlwSFlzAAAOxAAADsQBlSsOGwAAACtJREFUCJljYOBkZmZm5mBgYGBnYWLhZQUxeKEMHiZ+PiYuBgY2RhDgZgMAED4AwDb+hKoAAAAASUVORK5CYII="});
+
+/***/ }),
+
+/***/ 5118:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({"src":"/_next/static/media/tictactoe.b506bc4e.png","height":1080,"width":1920,"blurDataURL":"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAFCAMAAABPT11nAAAAKlBMVEUAAAB2dnb///88PDwzMzM7OzuxsbGGhoaxsrLx8vXR0dHX19fk5OTa290SuVcpAAAADHRSTlMCm/2nfp/GJN/6wNfKaCbPAAAACXBIWXMAAA7EAAAOxAGVKw4bAAAAKklEQVQImR3IyREAIAgAsZVDPKD/dh3JM2DqrgasrMwJXNki509lT4wWDxKiAM9sD+TTAAAAAElFTkSuQmCC"});
+
+/***/ }),
+
+/***/ 115:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_image__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(5675);
+/* harmony import */ var next_image__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_image__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(1664);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(next_link__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(6689);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_3__);
+
+
+
+
+const ProjectItem = ({ title , backgroundImg , tech , projectUrl , sourceUrl  })=>{
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+        className: "relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-[#c2c2c2] to-[#e4e4e4]",
+        children: [
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_image__WEBPACK_IMPORTED_MODULE_1___default()), {
+                className: "rounded-xl group-hover:opacity-10",
+                src: backgroundImg,
+                alt: "/"
+            }),
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                className: "hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]",
+                children: [
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("h3", {
+                        className: "text-2xl text-white tracking-wider text-center",
+                        children: title
+                    }),
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("p", {
+                        className: "pb-4 pt-2 text-white text-center",
+                        children: tech
+                    }),
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                        href: projectUrl,
+                        target: "_blank",
+                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("p", {
+                            className: "text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer",
+                            children: "View Site"
+                        })
+                    }),
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("br", {}),
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                        href: sourceUrl,
+                        target: "_blank",
+                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("p", {
+                            className: "text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer",
+                            children: "Source Code"
+                        })
+                    })
+                ]
+            })
+        ]
+    });
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (ProjectItem);
+
+
+/***/ }),
+
+/***/ 3191:
+/***/ ((__unused_webpack_module, __unused_webpack___webpack_exports__, __webpack_require__) => {
+
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_image__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(5675);
+/* harmony import */ var next_image__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_image__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(1664);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(next_link__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(6689);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_3__);
+
+
+
+
+const ProjectItems = ({ title , backgroundImg , tech , projectUrl  })=>{
+    return /*#__PURE__*/ _jsxs("div", {
+        className: "relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-[#c2c2c2] to-[#e4e4e4]",
+        children: [
+            /*#__PURE__*/ _jsx(Image, {
+                className: "rounded-xl group-hover:opacity-10",
+                src: backgroundImg,
+                alt: "/"
+            }),
+            /*#__PURE__*/ _jsxs("div", {
+                className: "hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]",
+                children: [
+                    /*#__PURE__*/ _jsx("h3", {
+                        className: "text-2xl text-white tracking-wider text-center",
+                        children: title
+                    }),
+                    /*#__PURE__*/ _jsx("p", {
+                        className: "pb-4 pt-2 text-white text-center",
+                        children: tech
+                    }),
+                    /*#__PURE__*/ _jsx("a", {
+                        href: projectUrl,
+                        target: "_blank",
+                        children: /*#__PURE__*/ _jsx("p", {
+                            className: "text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer",
+                            children: "View"
+                        })
+                    })
+                ]
+            })
+        ]
+    });
+};
+/* unused harmony default export */ var __WEBPACK_DEFAULT_EXPORT__ = ((/* unused pure expression or super */ null && (ProjectItems)));
+
+
+/***/ })
+
+};
+;
