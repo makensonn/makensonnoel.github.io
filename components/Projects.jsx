90a0722d9ee3eab0469f8817a3fe8d498bc733e8
Makenson Noel <22712773+makensonn@users.noreply.github.com>
2023-08-03 20:08:26 +0000

    Added the weather app to the project section

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -5,14 +5,14 @@ import { HiOutlineChevronDoubleUp } from 'react-icons/hi';
 
 
 //Image
-import projectOneImg from '../public/assets/projects/tictactoe.png'; // Tic Tac Toe
-import projectTwoImg from '../public/assets/projects/sudoku.png'; // Sudoku
+import projectOneImg from '../public/assets/projects/weatherfacts.png'; // Tic Tac Toe
+import projectTwoImg from '../public/assets/projects/tictactoe.png'; // Sudoku
 import projectThreeImg from '../public/assets/projects/dontforget.png'; // To Do List
 
 
 //Source
-const projectOneSource ='https://github.com/makensonn/tictactoe'; //Tic Tac Toe
-const projectTwoSource ='https://github.com/makensonn/makensonn.github.io/blob/master/components/Sudoku.jsx'; //Sudoku
+const projectOneSource ='https://github.com/makensonn/weatherfacts'; //Tic Tac Toe
+const projectTwoSource ='https://github.com/makensonn/tictactoe'; //Sudoku
 const projectThreeSource ='https://github.com/makensonn/todolist'; //To Do List
 
 
@@ -31,10 +31,28 @@ const Projects = () => {
         <p className='py-6'></p>
 
         <div className='grid md:grid-cols-2 gap-8'>
+
+        <ProjectItem
+            title='Weather App'
+            backgroundImg={projectOneImg}
+            projectUrl='https://weatherfacts.vercel.app/'
+            sourceUrl={projectOneSource}
+            tech=''
+          />       
+
+         {/*Project Description     */}
+
+            <p>Project: Weather Facts
+            <br></br><br></br>
+            Developed a weather application that fetches data from a weather API.
+            <br></br><br></br>
+            Technologies Used: React, JavaScript XML, TailwindCSS, Vercel, GitHub, Visual Studio Code</p>
+
+
         
           <ProjectItem
             title='Tic Tac Toe'
-            backgroundImg={projectOneImg}
+            backgroundImg={projectTwoImg}
             projectUrl='https://tictactoe-eta-tan.vercel.app/'
             sourceUrl={projectOneSource}
             tech=''
@@ -44,23 +62,10 @@ const Projects = () => {
 
             <p>Project: Tic Tac Toe
             <br></br><br></br>
-            • Developed a simple classic two-player board game played on a 3x3 grid.
+            Developed a simple classic two-player board game played on a 3x3 grid.
             <br></br><br></br>
-            • Technologies Used: React, JavaScript XML, TailwindCSS, Vercel, GitHub, Visual Studio Code</p>
+            Technologies Used: React, JavaScript XML, TailwindCSS, Vercel, GitHub, Visual Studio Code</p>
           
-          <ProjectItem
-            title='Sudoku Game'
-            backgroundImg={projectTwoImg}
-            projectUrl='/sud'
-            sourceUrl={projectTwoSource}
-            tech=''
-          /> 
-            <p>Project: Sudoku Game
-            <br></br><br></br>
-            • Developed a logic-based puzzle game for problem-solving.
-            <br></br><br></br>
-            • Technologies Used: React, JavaScript XML, TailwindCSS, Vercel, GitHub, Visual Studio Code</p>
-
           <ProjectItem
             title='Dont Forget'
             backgroundImg={projectThreeImg}
@@ -70,9 +75,9 @@ const Projects = () => {
           />
             <p>Project: Don't Forget
             <br></br><br></br>
-            • Developed a interactive to-do-list that stores and deletes items on a list.
+            Developed a interactive to-do-list that stores and deletes items on a list.
             <br></br><br></br>
-            • Technologies Used: Bootstrap, HTML, CSS, JavaScript, GitHub, Visual Studio Code</p>
+            Technologies Used: Bootstrap, HTML, CSS, JavaScript, GitHub, Visual Studio Code</p>
 
 
         {/*}  <ProjectItem
