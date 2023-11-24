d73e914624cafab863289b8a7cc0116bd153393a
Restyled.io <commits@restyled.io>
2023-11-24 03:04:17 +0000

    Restyled by prettier

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,25 @@
-import Head from 'next/head'
+import Head from "next/head";
 
-import About from '../components/About'
-import Main from '../components/Main'
-import Projects from '../components/Projects'
-import TicTacToe from '../components/TicTacToe'
+import About from "../components/About";
+import Main from "../components/Main";
+import Projects from "../components/Projects";
+import TicTacToe from "../components/TicTacToe";
 
 export default function Home() {
   return (
     <div>
       <Head>
         <title>Makenson Noel | Software Engineer </title>
-        <meta name="description" content="Award winning Software Engineer | Front-End Engineer| Back-End Engineer | Full-Stack Engineer | Web Developer | Software Developer | PC Builder" />
+        <meta
+          name="description"
+          content="Award winning Software Engineer | Front-End Engineer| Back-End Engineer | Full-Stack Engineer | Web Developer | Software Developer | PC Builder"
+        />
         <link rel="icon" href="/fav.png" />
       </Head>
-    <Main />
-    <About />
-    <Projects />
-    <TicTacToe />
+      <Main />
+      <About />
+      <Projects />
+      <TicTacToe />
     </div>
-   
-  )
+  );
 }
