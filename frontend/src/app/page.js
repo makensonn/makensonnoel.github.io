f02a50decc2fd5f02146db646f501193445c0e29
Restyled.io <commits@restyled.io>
2023-11-24 21:05:11 +0000

    Restyled by prettier

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -10,21 +10,27 @@ export default function Home() {
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
-  className =
-      "pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-  href =
-      "https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-  target = "_blank"
-  rel = "noopener noreferrer" > By{' '} < Image
-  src = "/vercel.svg"
-  alt = "Vercel Logo"
-  className = "dark:invert"
-  width = {100} height =
-      {24} priority / > </a>
-        </div><
-      /div>
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            By{" "}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className="dark:invert"
+              width={100}
+              height={24}
+              priority
+            />{" "}
+          </a>
+        </div>
+      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:
+      <div
+        className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:
               rounded - full before: bg - gradient - radial before: from -
           white before: to - transparent before: blur - 2xl before: content -
           [ '' ] after: absolute after: -z - 20 after: h - [ 180px ] after: w -
@@ -34,14 +40,17 @@ export default function Home() {
           br before: dark: from - transparent before: dark: to - blue -
           700 before: dark: opacity - 10 after: dark: from - sky -
           900 after: dark: via - [ #0141ff ] after: dark: opacity -
-          40 before: lg: h - [ 360px ] z - [ -1 ] ">
-      < Image
-  className = "relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-  src = "/next.svg"
-  alt = "Next.js Logo"
-  width = {180} height =
-      {37} priority / > <
-      /div>
+          40 before: lg: h - [ 360px ] z - [ -1 ] "
+      >
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />{" "}
+      </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
@@ -49,51 +58,46 @@ export default function Home() {
           nextjs.org / docs
       ? utm_source = create - next - app &utm_medium =
                          appdir - template &utm_campaign = create - next - app "
-  className =
-      "group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-  target = "_blank"
-  rel =
-      "noopener noreferrer" >
-          <h2 className = {`mb-3 text-2xl font-semibold`}>Docs{' '}<
-              span className =
-                  "inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">-
-      & gt;
-      </span>
-          </h2>
-      <p className = {`m-0 max-w-[30ch] text-sm opacity-50`}>Find in
-      - depth information about Next.js features and API.</p>
-        </a>
-
-      < a
-  href =
-      "https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-  className =
-      "group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-  target = "_blank"
-  rel =
-      "noopener noreferrer" >
-          <h2 className = {`mb-3 text-2xl font-semibold`}>Learn{' '}<
-              span className =
-                  "inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">-
-      & gt;
-      </span>
-          </h2>
-          <p className = {`m-0 max-w-[30ch] text-sm opacity-50`}>Learn about
-              Next.js in an interactive course with &
-      nbsp;
-      quizzes!</p>
-        </a>
-
-      < a
-  href =
-      "https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-  className =
-      "group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-  target = "_blank"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
+            Docs{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              - & gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Find in - depth information about Next.js features and API.
+          </p>
+        </a>
+
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Learn{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              - & gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Learn about Next.js in an interactive course with & nbsp; quizzes!
+          </p>
+        </a>
+
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Templates{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -104,15 +108,13 @@ export default function Home() {
         </a>
 
         <a
-          href =
-              "https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className =
-              "group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target = "_blank"
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
+            Deploy{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -123,5 +125,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
