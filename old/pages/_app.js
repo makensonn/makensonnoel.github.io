06f7ae4ca1d2eb939ebb70604b15fe564ab32d2d
Restyled.io <commits@restyled.io>
2023-11-27 08:04:13 +0000

    Restyled by clang-format

--- old/pages/_app.js
+++ old/pages/_app.js
@@ -1,12 +1,15 @@
-import Navbar from '../components/Navbar';
 import '../styles/globals.css';
-import { Analytics } from '@vercel/analytics/react';
 
-function MyApp({ Component, pageProps }) {
+import {Analytics} from '@vercel/analytics/react';
+
+import Navbar from '../components/Navbar';
+
+function MyApp({Component, pageProps}) {
   return (
     <>
       <Navbar />
-      <Component {...pageProps} />
+      <Component {
+    ...pageProps} />
       <Analytics />
     </>
   );
